ac8346ffe8046250c4056af59f6255aeaa14f186
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-12-21 12:04:43 +0700

    Initial commit

---
+++ lib/Perinci/Sub/DepUtil.pm
@@ -0,0 +1,58 @@
+package Perinci::Sub::DepUtil;
+
+use 5.010;
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+                       declare_function_dep
+               );
+
+# VERSION
+
+sub declare_function_dep {
+    my %args    = @_;
+    my $name    = $args{name}   or die "Please specify dep's name";
+    my $schema  = $args{schema} or die "Please specify dep's schema";
+    my $check   = $args{check};
+
+    $name =~ /\A\w+\z/
+        or die "Invalid syntax on dep's name, please use alphanums only";
+
+    require Rinci::Schema;
+    # XXX merge first or use Perinci::Object, less fragile
+    my $dd = $Rinci::Schema::function->[1]{"[merge+]keys"}{deps}
+        or die "BUG: Schema structure changed (1)";
+    $dd->[1]{keys}
+        or die "BUG: Schema structure changed (2)";
+    $dd->[1]{keys}{$name}
+        and die "Dependency type '$name' is already declared";
+    $dd->[1]{keys}{$name} = $args{schema};
+
+    if ($check) {
+        require Perinci::Sub::DepChecker;
+        no strict 'refs';
+        *{"Perinci::Sub::DepChecker::checkdep_$name"} = $check;
+    }
+}
+
+1;
+# ABSTRACT: Utility routines for Perinci::Sub::Dep::* modules
+
+=head1 SYNOPSIS
+
+
+=head1 FUNCTIONS
+
+=head2 declare_property
+
+
+=head1 SEE ALSO
+
+L<Perinci>
+
+Perinci::Sub::Dep::* modules.
+
+=cut
